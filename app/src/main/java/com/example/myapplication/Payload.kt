64f6e5de2ba098469d7ceee69c67b50c775601bf
0razzu <0razzu@protonmail.com>
2026-05-03 17:43:27 +0600

    nums to bytes

--- app/src/main/java/com/example/myapplication/Payload.kt
+++ app/src/main/java/com/example/myapplication/Payload.kt
@@ -1,14 +1,21 @@
 package com.example.myapplication
 
+import java.nio.ByteBuffer
 
-enum class PayloadType(private val serialized: String) {
-    DATA("d"), TEXT("t");
 
-    override fun toString(): String = serialized
+enum class PayloadType(private val serialized: Byte) {
+    DATA(0b00),
+    TEXT(0b01);
+
+    fun toByte(): Byte = serialized
+
+    companion object {
+        fun fromByte(b: Byte): PayloadType = entries.first { it.serialized == b }
+    }
 }
 
 
-abstract class Payload(val v: Int) {
+abstract class Payload(val v: Byte) {
     abstract fun toByteArray(): ByteArray
 }
 
@@ -17,5 +24,14 @@ class PayloadV1(
     val type: PayloadType,
     val data: ByteArray,
 ) : Payload(1) {
-    override fun toByteArray() = "$v$type${data.size}".toByteArray() + data
+    override fun toByteArray(): ByteArray {
+        val buf = ByteBuffer.allocate(1 + 1 + 4 + data.size)
+
+        buf.put(v)
+        buf.put(type.toByte())
+        buf.putInt(data.size)
+        buf.put(data)
+
+        return buf.array()
+    }
 }
